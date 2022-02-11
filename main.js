459385d6079f1f549a8f7200ad94c76cd3e87820
Alex Paiva <alexpaiva.dev@gmail.com>
2022-02-11 09:07:34 -0300

    11-02

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
+//ABRE E FECHA O MENU QUANDO CLICAR NO ICONE: HAMBURGUER
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show') //INSERE A CLASS 'SHOW' QUANDO CLICAR NO ICONE
   })
 }
 
+//ABRE E FECHA O MENU QUANDO CLICAR LINK
 const links = document.querySelectorAll('nav ul li a')
 
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
+  link.addEventListener('click', () => {
+    nav.classList.remove('show') //REMOVE A CLASS 'SHOW' QUANDO CLICA NO LINK
   })
 }
