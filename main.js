95cd20aed8eab221a387e24351514ebf84b596ea
Alex Paiva <alexpaiva.dev@gmail.com>
2022-03-15 09:05:14 -0300

    add_carousel

--- main.js
+++ main.js
@@ -29,3 +29,26 @@ window.addEventListener('scroll', () => {
     header.classList.remove('scroll')
   }
 })
+
+// START OWN CAROUSEL
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    items: 4,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 4
+      }
+    }
+  })
+})
